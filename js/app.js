391a8725f488e6078b4642c5e6f0a169d34e49f4
carlito-jdp <carlito.jdp@gmail.com>
2021-01-29 11:12:16 +0000

    Minor changes to fetch functuon.

--- js/app.js
+++ js/app.js
@@ -66,20 +66,6 @@ const searchPokemon = async name => {
         });
 }
 
-function myFetch(url, options) {
-    if (options == null) options = {}
-    if (options.credentials == null) options.credentials = 'same-origin'
-    return fetch(url, options).then(function(response) {
-      if (response.status >= 200 && response.status < 300) {
-        return Promise.resolve(response)
-      } else {
-        var error = new Error(response.statusText || response.status)
-        error.response = response
-        return Promise.reject(error)
-      }
-    })
-  }
-
 submitForm.addEventListener('click', e => {
     e.preventDefault();
     const search = pokemonInput.value.trim();
