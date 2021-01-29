e1a1c3647fceb5910ca374039c2a7f2d2c99bf64
carlito-jdp <carlito.jdp@gmail.com>
2021-01-29 00:54:39 +0000

    Created Pokedex clone with scss and grid template.

---
+++ js/app.js
@@ -0,0 +1,97 @@
+const pokedex = document.getElementById('pokedex');
+const showSearch = document.getElementById('show-search');
+const errorMsg = document.getElementById('error-msg');
+const pokemonInput = document.getElementById("form__input");
+const submitForm = document.getElementById("form__submit");
+
+const fetchPokemon = async () => {
+    const promises = [];
+    for (let i=1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then(res => res.json()));
+    }
+    Promise.all(promises).then(results => {
+        const pokemon = results.map(data => {
+            const {id, name, sprites} = data;
+            return ({
+                id,
+                name,
+                img: sprites['front_default'],
+                type: data.types.map(type => type.type.name).join(', '),
+            })
+        })
+        displayPokemon(pokemon)
+    })
+};
+
+const displayPokemon = (pokemons) => {
+    const pokemonHTMLString = pokemons.map(pokemon => {
+        const {id, name, img, type} = pokemon;
+        return (`
+            <li class="card">
+                <img src="${img}" class="card__img"/>
+                <h2 class="card__title">${id}. ${name}</h2>
+                <p class="card__subtitle">Type: ${type}</p>
+            </li>
+        `)
+    }).join("");
+    pokedex.innerHTML = pokemonHTMLString;
+}
+
+const searchPokemon = async name => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+    await fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            errorMsg.style.display = "none";
+            const {id, name, sprites} = data;
+            const pokemon = {
+                id,
+                name,
+                img: sprites["front_default"],
+                type: data.types.map(type => type.type.name).join(', '),
+            }
+            const showPokemonHTML = `
+            <li class="card">
+                <img src="${pokemon.img}" class="card__img"/>
+                <h2 class="card__title">${pokemon.id}. ${pokemon.name}</h2>
+                <p class="card__subtitle">Type: ${pokemon.type}</p>
+            </li>
+            `;
+            showSearch.innerHTML = showPokemonHTML;
+        })
+        .catch(err => {
+            errorMsg.style.display = "block";
+            errorMsg.textContent = `Cannot find ${name}... please try again.`
+        });
+}
+
+function myFetch(url, options) {
+    if (options == null) options = {}
+    if (options.credentials == null) options.credentials = 'same-origin'
+    return fetch(url, options).then(function(response) {
+      if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response)
+      } else {
+        var error = new Error(response.statusText || response.status)
+        error.response = response
+        return Promise.reject(error)
+      }
+    })
+  }
+
+submitForm.addEventListener('click', e => {
+    e.preventDefault();
+    const search = pokemonInput.value.trim();
+    if (search.length == 0 ) {
+        errorMsg.style.display = "block";
+        errorMsg.textContent = `Enter a pokemon here... Cannot be blank`
+        pokemonInput.value = "";
+    } else {
+        searchPokemon(search);
+    }
+})
+
+document.addEventListener("DOMContentLoaded", e => { 
+    fetchPokemon();
+  });
